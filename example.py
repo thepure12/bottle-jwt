4f3fd0fd5f8e866a05dc29bd8d4f6cbf8dd772cb
TJ Renninger <renningert@LAPTOP-LLEJQRTO.localdomain>
2023-05-03 10:30:24 -0400

    added postman testing

---
+++ example.py
@@ -0,0 +1,38 @@
+from bottle import Bottle, request
+from src.bottle_jwt import JWTPlugin
+from datetime import datetime, timedelta
+
+app = Bottle()
+
+def auth():
+    return { "roles": ["guest"], "exp": datetime.utcnow() + timedelta(minutes=5) }
+
+plugin = JWTPlugin("changeme", auth_func=auth, debug=True)
+plugin.addTokenPath("token2", auth_func=auth)
+app.install(plugin)
+
+@app.route("/token", method=["GET", "POST"])
+def token():
+    pass
+
+@app.route("/token2", method=["GET", "POST"])
+def token2():
+    pass
+
+@app.route("/user", "GET")
+def user():
+    return {"user": request.user}
+
+@app.route("/callable_roles/list", roles=lambda: ["guest", "user"])
+def gen_list():
+    return {"msg": "Generated list of roles"}
+
+@app.route("/callable_roles/bool", roles=lambda: True)
+def gen_bool():
+    return {"msg": "Generated a boolean"}
+
+@app.route("/token_protected", roles=True)
+def permiss_func():
+    return {"msg": "Used the check roles function"}
+
+app.run( host="localhost", debug=True, reloader=True)
